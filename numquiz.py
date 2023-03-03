9c4ba5004909b4097f9e1513a36ff87d50f68fa3
whiskeyZZ <julian.ortmann@gmx.de>
2023-03-03 14:29:03 -0500

    some approvements

--- numquiz.py
+++ numquiz.py
@@ -21,11 +21,14 @@ class Numquiz:
         self.question_lbl = tk.Label(window, wraplength=400, font=("Arial", 22), fg="white", bg="black")
         self.question_lbl.pack(pady=20)
 
-        self.entry = tk.Entry(window, font=("Helvetica", 36))
+        self.entry = tk.Entry(window, font=("Helvetica", 36, "italic"))
         self.entry.pack(pady=20)
 
         self.button = tk.Button(window, text="Antwort", bg="cornflower blue", fg="white",font=("Arial", 20) ,command=lambda: self.set_question(False))
         self.button.pack()
+
+        self.info_lbl = tk.Label(window, font=("Arial", 22), fg="white", bg="black")
+        self.info_lbl.pack(pady=20)
 
         self.set_question(True)
 
@@ -37,11 +40,14 @@ class Numquiz:
                 self.question = self.quest[self.i]["question"]
                 self.answer =  self.quest[self.i]["answer"]
                 self.question_lbl.config(text=self.question)
+                self.info_lbl.config(text="")
                 self.entry.delete(0, tk.END)
                 self.i += 1
             else:
                 self.question_lbl.config(text="Alles Richtig!")
                 self.entry.delete(0, tk.END)
+        elif self.i < len(self.quest):
+            self.info_lbl.config(text="Falsche Antwort")
 
 
 if __name__ == "__main__":
